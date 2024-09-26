d38b6b446d79c12d17f390218955f3b8888caf6a
jean TROUSSIER <jean.troussier@cpe.fr>
2024-09-26 11:18:32 +0200

    version finale

--- main.py
+++ main.py
@@ -3,4 +3,27 @@ auteur : jean TROUSSIER
 date : 26 sept 2024
 objectif : ici seront mis en place l'organisation des différents appel de fonction
 """
- 
+import input_function as ifu
+import computing_function as cf
+
+while (1):
+    string_saise = input("entrez une phrase a corriger :>")
+    list_string_saise = ifu.liste_phrase_spliter(string_saise)
+    if ifu.bool_phrase_vide(list_string_saise) == True:
+        print("erreur, votre phrase est fausse")
+        break
+    if ifu.bool_point_checker(list_string_saise) ==False:
+        print("erreur veuiller mettre un point pour signifier la fin de votre phrase")
+        break
+    list_string_saise = ifu.string_final_point_separator(ifu.string_point_seperator(list_string_saise))
+    list_int_saise = cf.int_list_nombre_associé(list_string_saise)
+    if list_int_saise == [1]:
+        print("voter mot ne semble pas exister dans nos registre...")
+        break
+    if cf.bool_etat_de_la_phrase(list_int_saise) == True:
+        print("votre phrase est juste")
+        break
+    else:
+        print("votre phrase est incorrect")
+        break
+
--- test_function.py
+++ test_function.py
@@ -7,4 +7,4 @@ import input_function as ifu
 
 print(ifu.liste_phrase_spliter("je vais bien mais ej me senes paraiel ."))
 print(ifu.bool_point_checker(['hello','cool.']))
-print(ifu.string_point_seperator(['hello','cool.','hello','world']))
+print(ifu.string_final_point_separator(ifu.string_point_seperator(['hello','cool.','hello','world'])))
