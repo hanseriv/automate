ef8b1c3ddb3c812b25d751ad405a166705cc89d0
jean TROUSSIER <jean.troussier@cpe.fr>
2024-09-26 10:20:31 +0200

    commencement des fonctions de traitement

--- computing_function.py
+++ computing_function.py
@@ -3,3 +3,6 @@ auteur : jean TROUSSIER
 date : 26 sept 2024
 objectif : ici seront coder toutes les fonctions prenant lié au traitement des donnée saisie et vérifié
 """
+mat_int_etat = [[1,8,8,8,4,8],[8,1,3,8,8,8],[8,2,8,3,8,8],[5,8,8,8,7,9],[8,8,8,3,8,8],[8,5,6,8,8,8],[8,6,8,8,8,9],[8,8,8,8,8,9]]
+
+def bool_()
