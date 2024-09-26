308e65335001a233bec142a97a5185688397874b
jean TROUSSIER <jean.troussier@cpe.fr>
2024-09-26 10:36:05 +0200

    table de vérité préte

--- computing_function.py
+++ computing_function.py
@@ -5,4 +5,18 @@ objectif : ici seront coder toutes les fonctions prenant lié au traitement des 
 """
 mat_int_etat = [[1,8,8,8,4,8],[8,1,3,8,8,8],[8,2,8,3,8,8],[5,8,8,8,7,9],[8,8,8,3,8,8],[8,5,6,8,8,8],[8,6,8,8,8,9],[8,8,8,8,8,9]]
 
-def bool_()
+def bool_etat_de_la_phrase(list_int_indice_mot):
+    """
+    input : list_int_indice_mot
+    output : True/False
+    fonction de vérification de l'ordre des mots
+    """
+    int_cas = 0
+    for int_valeur in list_int_indice_mot:
+        int_cas = mat_int_etat[int_cas][int_valeur]
+        if int_cas == 8:
+            return False
+        if int_cas == 9:
+            return True
+    print("error during runtime please notify yourself to the previous programmer")
+    return False
